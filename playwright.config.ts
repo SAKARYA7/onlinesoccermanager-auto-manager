bb49003fa384f5b32dd1c6f8858c275d3983ce5b
Omar Karim Chtioui <omar.karim.chtioui-cic-it@ibm.com>
2022-10-26 17:11:35 +0200

    refactor(playwright config): remove redundant code

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,11 +4,7 @@ import dotenv from 'dotenv';
 import path from 'path';
 
 // Read from default ".env" file.
-dotenv.config();
-
-// Alternatively, read from "../my.env" file.
 dotenv.config({ path: path.resolve(__dirname, '.', '.env') });
-
 
 /**
  * See https://playwright.dev/docs/test-configuration.
